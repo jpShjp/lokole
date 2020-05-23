6b547d1ca014341cdeae66de87bf3a97990c93d0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-23 23:24:23 +0000

    Deploy ascoderu/lokole to github.com/ascoderu/lokole.git:gh-pages

---
+++ lokole/precache-manifest.8d21a3abb18c1225a7a43089093330e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079ce8f4307103e3db98fb4243632320",
+    "url": "/lokole/index.html"
+  },
+  {
+    "revision": "87a558775f4a69779655",
+    "url": "/lokole/static/css/2.63aa2317.chunk.css"
+  },
+  {
+    "revision": "87a558775f4a69779655",
+    "url": "/lokole/static/js/2.43b111c4.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/lokole/static/js/2.43b111c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ca72f95bbc430e1799",
+    "url": "/lokole/static/js/main.8dff751d.chunk.js"
+  },
+  {
+    "revision": "2ee7d9ddc9b2840700cb",
+    "url": "/lokole/static/js/runtime-main.e17e7094.js"
+  }
+]);
